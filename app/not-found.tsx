790cc5e61ddd87eb99c547e54db04c708f6dd1a9
Scott <kizmetsol@proton.me>
2025-10-25 15:18:55 +0900

    remove

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic';
+
 export default function NotFound() {
   return (
     <div className="mx-auto w-full max-w-3xl px-6 py-12">
