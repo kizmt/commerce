73492631c244e241b1cbe1ac9e02007e7ad34523
Scott <kizmetsol@proton.me>
2025-10-31 21:54:48 +0900

    update-seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ const siteDescription =
 export const metadata = {
   metadataBase: new URL(baseUrl.endsWith("/") ? baseUrl : `${baseUrl}/`),
   title: {
-    default: `${siteName} | TCG NZ | Pokémon, MTG, One Piece`,
+    default: `${siteName}`,
     template: `%s | ${siteName}`,
   },
   description: siteDescription,
